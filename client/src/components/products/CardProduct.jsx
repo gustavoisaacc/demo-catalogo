2809f89a618f421b07d2f1c51bab9b3b3fb1099e
gustavoisaacc <gustavochaile7@gmail.com>
2024-09-05 21:50:19 -0300

    change in card

--- client/src/components/products/CardProduct.jsx
+++ client/src/components/products/CardProduct.jsx
@@ -10,6 +10,7 @@ export default function Card({ items }) {
     height: window.innerHeight,
   });
   const lo = items.name.toUpperCase();
+
   useEffect(() => {
     function handleResize() {
       setWindowSize({
@@ -22,23 +23,23 @@ export default function Card({ items }) {
 
     return () => window.removeEventListener("resize", handleResize);
   }, [windowSize]);
-  // Memorizar la función para evitar su recreación en cada renderizado
+
   const handleAvailability = useCallback(
     async (id) => {
       await updateAvailable(id);
       await getProduct(); // Podrías optimizar esto actualizando solo el producto, no todos
     },
-    [updateAvailable, getProduct]
+    [updateAvailable, getProduct, windowSize]
   );
 
   return (
     <div
       className={`bg-white min-h-[300px] min-w-40 relative group overflow-hidden rounded-lg shadow-lg hover:shadow-xl transition-transform duration-300 ease-in-out hover:-translate-y-2 ${
         !items.availability ? "grayscale cursor-not-allowed" : "hover:scale-105"
-      }  md:max-w-full`}
+      } md:max-w-full`}
     >
       {!items.availability && (
-        <p className=" w-full py-1 absolute top-0 left-0 flex justify-center items-center text-md text-white font-bold bg-red-950 rounded-lg">
+        <p className="w-full py-1 absolute top-0 left-0 flex justify-center items-center text-md text-white font-bold bg-red-950 rounded-lg">
           Sin Stock
         </p>
       )}
@@ -48,22 +49,20 @@ export default function Card({ items }) {
         loading="lazy" // Lazy loading de la imagen
         src={items.image}
         alt={`imagen de ${items.name}`}
-        className="w-full h-full rounded-lg shadow-md transition-transform duration-300"
+        className="w-full h-full object-cover rounded-t-lg"
       />
-      <div className="bg-white p-4 dark:bg-[#333333]">
-        <div className="flex justify-between md:block">
-          <h3 className="font-bold text-sm md:text-md text-[#333333] dark:text-white ]">
-            {windowSize.width < 700
-              ? lo.length > 18
-                ? lo.slice(0, 15) + "..."
-                : lo
-              : lo}
-          </h3>
-          <h4 className="font-semibold text-lg md:text-md text-secondaryDarck">
-            ${items.price}
-          </h4>
-        </div>
-        <p className="text-sm text-[#333333] dark:text-white ] ">
+      <div className="bg-white p-4 dark:bg-[#333333] flex flex-col justify-between h-full">
+        <h4 className="font-semibold text-lg md:text-md text-secondaryDarck ">
+          ${items.price}
+        </h4>
+        <h3 className="font-bold text-sm md:text-md text-[#333333] dark:text-white truncate">
+          {windowSize.width < 700
+            ? lo.length > 18
+              ? lo.slice(0, 15) + "..."
+              : lo
+            : lo}
+        </h3>
+        <p className="text-sm text-[#333333] dark:text-white mt-2">
           {items.description}
         </p>
       </div>
@@ -71,7 +70,7 @@ export default function Card({ items }) {
       {/* Mostrar el botón de stock solo si el usuario está autenticado */}
       {isAuth && (
         <div
-          className="text-center text-2xl py-2 text-white bg-secondary hover:bg-secondaryDarck cursor-pointer"
+          className="text-center text-2xl py-2 text-white bg-secondary hover:bg-secondaryDark cursor-pointer"
           onClick={() => handleAvailability(items._id)}
         >
           Stock
